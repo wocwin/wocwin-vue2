1311d13a6b4a2e61d988442a9aff99017f36453c
wocwin <wocwin@163.com>
2024-08-28 20:49:41 +0800

    feat:新增大屏路由

---
+++ src/views/largeScreen/workshop/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<t-layout-page>
+		<t-layout-page-item>车间大屏</t-layout-page-item>
+	</t-layout-page>
+</template>
+<script>
+export default {
+  name: '',
+  data() {
+    return {}
+  },
+  computed: {},
+  created() { },
+  methods: {
+
+  }
+}
+</script>
